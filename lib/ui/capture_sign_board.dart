276f20fb3598a214102fedceae66755e0a9c01d5
Tilo <tiloc@users.noreply.github.com>
2022-07-17 00:52:19 +0200

    Pulled in a few plugins

---
+++ lib/ui/capture_sign_board.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CaptureSignBoard extends StatefulWidget {
+  const CaptureSignBoard({super.key});
+
+  @override
+  State<CaptureSignBoard> createState() => _CaptureSignBoardState();
+}
+
+class _CaptureSignBoardState extends State<CaptureSignBoard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
