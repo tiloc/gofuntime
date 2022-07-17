868c6effa619a5d804b9cfad8c7f6af6835bbff3
Tilo <tiloc@users.noreply.github.com>
2022-07-18 00:09:26 +0200

    - Add font - Add camera

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+import 'package:camera/camera.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:gofuntime/ui/capture_sign_board.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main() {
+List<CameraDescription> cameras = <CameraDescription>[];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // TODO: This is shitty. Camera View should be self-contained.
+  cameras = await availableCameras();
+
   runApp(const FuntimeApp());
 }
 
@@ -12,7 +20,7 @@ final _router = GoRouter(
     GoRoute(
       path: '/',
       name: 'home',
-      builder: (context, state) => const CaptureSignBoard(),
+      builder: (context, state) => CaptureSignBoard(cameras: cameras,),
     ),
   ],
 );
@@ -30,11 +38,11 @@ class FuntimeApp extends StatelessWidget {
           theme: ThemeData(
             colorScheme: lightColorScheme,
             useMaterial3: true,
-          ),
+          ).copyWith(textTheme: GoogleFonts.indieFlowerTextTheme(),),
           darkTheme: ThemeData(
             colorScheme: darkColorScheme,
             useMaterial3: true,
-          ),
+          ).copyWith(textTheme: GoogleFonts.indieFlowerTextTheme(),),
           routeInformationProvider: _router.routeInformationProvider,
           routeInformationParser: _router.routeInformationParser,
           routerDelegate: _router.routerDelegate,
